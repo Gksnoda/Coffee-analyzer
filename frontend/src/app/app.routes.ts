d7372de9080f4e3a66ae227ef190c796f1bd8349
Gksnoda <gksnoda@hotmail.com>
2023-10-02 00:50:14 -0300

    Mexendo nas rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { NavComponent } from 'libs/master-page/nav/nav.component';
+import { TestandoComponent } from './testando/testando.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {path: '', component: NavComponent},
+    {path: 'teste', component: TestandoComponent}
+];
